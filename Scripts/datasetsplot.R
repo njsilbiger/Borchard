bbe09af0e0994ff5f19ff932de85ea8a919d3064
njsilbiger <silbiger@hawaii.edu>
2025-06-10 15:46:06 +0200

    minor changes to the plot

--- Scripts/datasetsplot.R
+++ Scripts/datasetsplot.R
@@ -18,8 +18,9 @@ data<- read_csv(here("Data","bar_graph_data_entry.csv"))
 
 pal <- wes_palette("Zissou1", 15, type = "continuous")
 
-ggplot(data, aes(year, log(number_sites+1)*bar_value, linetype = gump_criobe)) +
-  geom_area(aes(fill = dataset))+
+ggplot(data, aes(year, log(number_sites+1)*bar_value, 
+                 linetype = gump_criobe)) +
+  geom_area(aes(fill = dataset), color = "black")+
   #geom_hline(yintercept = 0, yend = 2025,linewidth = 1)+
   geom_segment(aes(y = 0, yend = 0, x = 1980, xend = 2025), size = 1)+
   annotate(geom = "text", x = 1990, y = 20,label = "Gump", size = 8)+
@@ -28,6 +29,7 @@ ggplot(data, aes(year, log(number_sites+1)*bar_value, linetype = gump_criobe)) +
        x = "",
        fill = "")+
   scale_fill_manual(values = pal)+
+  scale_linetype_manual(values = c(1,1))+
   guides(linetype = "none")+
   theme_classic()+
   theme(axis.text.y = element_blank(),
@@ -35,5 +37,5 @@ ggplot(data, aes(year, log(number_sites+1)*bar_value, linetype = gump_criobe)) +
         axis.text.x = element_text(size = 14),
         axis.title.y = element_text(size = 14))
 
-ggsave(here("Output","datasets.png"), height = 5, width = 7)
+ggsave(here("Output","datasets.png"), height = 4, width = 8)
 
